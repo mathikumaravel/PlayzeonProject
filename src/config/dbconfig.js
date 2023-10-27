90272776e71f2869566e6f225db888e4038c422f
mathikumaravel <shanmathiemail@gmail.com>
2023-10-27 18:00:54 +0530

    updatedorga

--- src/config/dbconfig.js
+++ src/config/dbconfig.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
  
-const sequelize = new Sequelize('playzeondb', 'postgres','Password@11', {
+const sequelize = new Sequelize('play', 'postgres','Password@11', {
   host: 'localhost',
   port: '5432',
   // schema:'playzeon',
--- src/models/index.js
+++ src/models/index.js
@@ -3,11 +3,11 @@ const sequelize = require("../config/dbconfig");
 const db = {};
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-db.Organizations = require("./organizations.model")(sequelize, Sequelize);
+db.Organizations = require("./organizations.model");
 // db.OrganizationUser = require("./organizationUser.model")(sequelize, Sequelize);
 // db.UserDetails = require("./userDetails.model")(sequelize,Sequelize);
 // db.Organizations.belongsTo(db.OrganizationUser, {
 //     foreignKey: { name: "organization_id" },
 //   });
-sequelize.sync({ alter: true });
+sequelize.sync({force: true});
 module.exports = db;
